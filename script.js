5218d8aa8ac1a6e739267c08d5269a67fd9de3cf
Marcus Garnot <marcusgarnot@Marcuss-Air.lan>
2024-01-15 15:29:08 -0500

    draft files

---
+++ script.js
@@ -0,0 +1,67 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+
+
+function search(str) {
+	//this event listener will listen for the key last released by the user and run searchHandler
+	let results = [];
+     input.addEventListener("keyup", searchHandler)
+
+	return results;
+}
+
+
+function searchHandler(e) {
+	//This function is triggered on every keyup event in the search bar. Its main goal is to filter the list
+	// of fruits based on the user's input and then display the relevant suggestions.
+
+	const inputText = input.value;
+	const lowerCaseInput = inputText.toLowerCase();
+	let results = [];
+	for ( let fruit of fruits){
+		if (fruit.toLowerCase().includes(lowerCaseInput)) {
+			 results.push(fruit);}
+	}
+     return showSuggestions(results,lowerCaseInput);
+		
+} 
+
+lowerCaseInput.reset();
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry',
+'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa',
+'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 
+'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon',
+'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine',
+'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince',
+'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind',
+'Yuzu'];
+
+function showSuggestions(results, inputVal) {
+	//This function takes the filtered results and the current input value, then updates the UI to show these suggestions in a dropdown.
+  suggestions.innerHTML = ""; 
+  //let ul= document.createElement('ul');
+  for(let fruit in results){
+	let listItem = document.createElement('li');
+	listItem.textContent = fruit;
+	listItem.addEventListener("mouseover", (e) => {
+		e.target.style.color = "orange";
+
+	})
+	//ul.appendChild('li');
+	suggestions.appendChild('li');
+  }
+  
+}
+
+
+function useSuggestion(e) {
+	//This function is called when a suggestion in the dropdown is clicked. It should update the search bar with the clicked suggestion.
+	const clickedFruit = e.target.textContent;
+	input.value = clickedFruit;
+	suggestions.innerHTML = '';
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
